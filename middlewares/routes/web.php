0b6c8e30190f015bb0adf07978a80ffad23191a5
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-03-02 12:28:24 -0300

    Adicionando novo middleware

--- middlewares/routes/web.php
+++ middlewares/routes/web.php
@@ -4,6 +4,10 @@
 
 // Route::get('/usuarios', 'UsuarioControlador@Index')->middleware('primeiro');
 
+Route::get('/usuarios', 'UsuarioControlador@Index');
+
 Route::get('/', function (){
     return 'teste2';
 });
+
+// Route::get('/usuarios', 'UsuarioControlador@Index')->middleware(['primeiro', 'segundo']);
