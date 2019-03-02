8e5e282dc4d13daa0d997e80ba2fa62094f7daaa
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-03-02 13:20:46 -0300

    Implementando um login básico através de middlewares

--- middlewares-login/routes/web.php
+++ middlewares-login/routes/web.php
@@ -1,18 +1,33 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/produtos', 'ProdutoControlador@Index');
+
+Route::post('/login', function (Request $req) {
+
+    $login_ok = false;
+
+    switch ($req->input('user')) {
+        case 'joao':
+        $login_ok = $req->input('passwd') === "senhajoao";
+            break;
+        case 'marcos':
+        $login_ok = $req->input('passwd') === "senhamarcos";
+            break;
+        default:
+        $login_ok = false;
+            break;
+    }
+
+    if($login_ok) {
+        return response("Login OK", 200);
+    }
+    else{
+        return response("Erro no login", 404);
+    }
+});
