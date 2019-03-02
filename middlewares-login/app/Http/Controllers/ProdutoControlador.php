e146e734745acff45e3bf834487126965c5e90ad
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-03-02 13:12:55 -0300

    Middleware - Login

---
+++ middlewares-login/app/Http/Controllers/ProdutoControlador.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutoControlador extends Controller
+{
+    private $produtos = [
+        "Televisao 40",
+        "Notebook Dell G3",
+        "Impressora HP",
+        "HD Externo"
+    ];
+
+    public function Index() {
+        
+        echo "<h3>Produtos</h3>";
+            echo "<ol>";
+            foreach ($this->produtos as $p) {
+                echo "<li> $p </li>";
+            } 
+            echo"<ol>";
+    }
+}
