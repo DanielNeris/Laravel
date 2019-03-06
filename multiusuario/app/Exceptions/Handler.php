0def82c5c2c5729c71b7c61e3be75c9662ce0d6b
Daniel de Souza Neris <daniel@liveonsolutions.com>
2019-03-06 18:37:45 -0300

    Resolvendo Problema 2

--- multiusuario/app/Exceptions/Handler.php
+++ multiusuario/app/Exceptions/Handler.php
@@ -51,7 +51,25 @@ class Handler extends ExceptionHandler
     }
 
     protected function unauthenticated($request, AuthenticationException $exception) {
+
+        if ($request->expectsJson()) {
+            return response()->json(['message' => $exception->getMessage()], 401);
+        }
+
         $guard = array_get($exception->guards(), 0);
-        return response("Oi $guard");
+
+        switch ($guard) {
+            case 'admin':
+                $login = "admin.login";
+                break;
+            case 'web':
+                $login = "login";
+                break;
+            default:
+                $login = "login";
+                break;
+        }
+
+        return redirect()->guest(route($login));
     }
 }
